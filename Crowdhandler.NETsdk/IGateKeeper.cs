5051c830b218c952d2ca8a4e559e6f1ffaa26f10
lukeowen89 <luke.owen@crowdhandler.com>
2023-02-07 12:33:36 +0000

    Support to provide URL pattern exclusions via regex and a common sense default exclusion pattern

--- Crowdhandler.NETsdk/IGateKeeper.cs
+++ Crowdhandler.NETsdk/IGateKeeper.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Crowdhandler.NETsdk
@@ -14,5 +15,6 @@ namespace Crowdhandler.NETsdk
         String PublicApiKey { get; set; }
         String PrivateApiKey { get; set; }
         String ApiEndpoint { get; set; }
+        String Exclusions { get; set; }
     }
 }
